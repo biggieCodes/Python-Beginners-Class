a4639c3afc907c5bdde29d895f03521b55f99aba
Abdulsamad <you@example.com/usr/bin/python>
2025-04-07 13:13:54 +0100

    new commit

---
+++ Python Classes/builtin_input.py
@@ -0,0 +1,8 @@
+#input§
+
+input()
+print(input("Enter your name: "))
+
+tomiwa_age = input("Enter Tomiwa's age: ")
+your_age = tomiwa_age + 10
+print("if Tomiwas is", tomiwa_age, "then you are", your_age, "years old")
