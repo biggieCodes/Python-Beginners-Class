ead4b909e4f4fd2d5353331e827f3571ac6dd149
Abdulsamad <you@example.com/usr/bin/python>
2025-04-08 18:27:33 +0100

    Refactored f-strings and added age and cgpa calculations

--- Python Classes/Private Practice/f_strings.py
+++ Python Classes/Private Practice/f_strings.py
@@ -5,3 +5,11 @@ result: float = 30
 print("Farida, how could you get", result, "in your english?")
 print()
 print(f"Farida, how could you get {result} in your english?")
+
+
+age: float = 52
+cgpa: float = 0.3
+
+print("kunle you are now ", age, "years old, yet your cgpa is still", cgpa)
+print()
+print(f"kunle you are now {age} years old, yet your cgpa is still {cgpa}")
