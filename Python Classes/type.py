443e1f1e962706c66d132313753e886b4a5a6650
Abdulsamad <you@example.com/usr/bin/python>
2025-04-08 20:53:55 +0100

    Fix variable name and correct usage of 'type' function

--- Python Classes/type.py
+++ Python Classes/type.py
@@ -1,23 +1,26 @@
 # type
 
+# Example of a tuple
 x = 3
 y = 5.0
 z = 3e2
 
-type_x = type(type_x)
+# Correcting the variable name to avoid overwriting the built-in 'type' function
+type_x = type(x)
 print(type_x)
 
+# Correcting the usage of 'type' function
 print(type(x + y / z))
 
-# #Example of a list with a map function
+# Example of a list with a map function
 # my_map_list = list(map(lambda x: x * 2, range(1, 6)))
-# print(my_map_list) #Output: [2, 4, 6, 8, 10] = type(x)
+# print(my_map_list) # Output: [2, 4, 6, 8, 10]
 
-# #Example of a list with a filter function
+# Example of a list with a filter function
 # my_filter_list = list(filter(lambda x: x % 2 == 0, range(1, 11)))
-# print(my_filter_list) #Output: [2, 4, 6, 8, 10]
+# print(my_filter_list) # Output: [2, 4, 6, 8, 10]
 
-# #Example of a list with a reduce function
+# Example of a list with a reduce function
 from functools import reduce
 my_reduce_list = reduce(lambda x, y: x + y, range(1, 6))
 print(my_reduce_list) #Output: 15
@@ -30,7 +33,6 @@ print(my_zip_list) #Output: [(1, 6), (2, 7), (3, 8), (4, 9), (5, 10)]
 my_map_list = list(map(lambda x: x * 2, range(1, 6)))
 print(my_map_list) #Output: [2, 4, 6, 8, 10]
 #
-
 
 #Example of a list with a filter function
 my_filter_list = list(filter(lambda x: x % 2 == 0, range(1, 11)))
