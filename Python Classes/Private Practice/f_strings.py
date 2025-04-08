225a71dcb5a15183e27f8454c60a5cddd79abc47
Abdulsamad <you@example.com/usr/bin/python>
2025-04-08 14:53:04 +0100

    Refactor variable names and update comments

---
+++ Python Classes/Private Practice/f_strings.py
@@ -0,0 +1,7 @@
+#Read Strings methods and f-strings
+result: float = 30
+
+#Farida, how could you get 30 in your english?
+print("Farida, how could you get", result, "in your english?")
+print()
+print(f"Farida, how could you get {result} in your english?")
