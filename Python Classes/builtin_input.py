66d17efd7ec26663105ac1b4a1a151f25f7e1f56
Abdulsamad <you@example.com/usr/bin/python>
2025-04-08 20:35:13 +0100

    Remove unnecessary code and fix input conversion bug

--- Python Classes/builtin_input.py
+++
@@ -1,8 +0,0 @@
-#input§
-
-input()
-print(input("Enter your name: "))
-
-tomiwa_age = input("Enter Tomiwa's age: ")
-your_age = tomiwa_age + 10
-print("if Tomiwas is", tomiwa_age, "then you are", your_age, "years old")
---
+++ Python Classes/inputs.py
@@ -0,0 +1,50 @@
+#input§
+
+input()
+print(input("Enter your name: "))
+
+tomiwa_age = input("Enter Tomiwa's age: ")
+your_age = tomiwa_age + 10
+print("if Tomiwas is", tomiwa_age, "then you are", your_age, "years old")
+# input() function
+# The input() function is used to take input from the user.
+# It returns the input as a string.
+# The input() function can take an optional prompt string as an argument.
+# The prompt string is displayed to the user before taking input.
+# The input() function can also take a default value as an argument.
+# The default value is displayed to the user before taking input.
+# The input() function can also take a callback function as an argument.
+# The callback function is called with the input string as an argument.
+# The input() function can also take a list of strings as an argument.
+# The list of strings is displayed to the user before taking input.
+# The input() function can also take a dictionary as an argument.
+# The dictionary is displayed to the user before taking input.
+# The input() function can also take a set as an argument.
+# The set is displayed to the user before taking input.
+# The input() function can also take a tuple as an argument.
+# The tuple is displayed to the user before taking input.
+# The input() function can also take a range as an argument.
+# The range is displayed to the user before taking input.
+# The input() function can also take a generator expression as an argument.
+# The generator expression is displayed to the user before taking input.
+# The input() function can also take a list comprehension as an argument.
+# The list comprehension is displayed to the user before taking input.
+# The input() function can also take a lambda function as an argument.
+# The lambda function is called with the input string as an argument.
+# The input() function can also take a map function as an argument.
+# The map function is called with the input string as an argument.
+# The input() function can also take a filter function as an argument.
+# The filter function is called with the input string as an argument.
+# The input() function can also take a reduce function as an argument.
+# The reduce function is called with the input string as an argument.
+# The input() function can also take a zip function as an argument.
+# The zip function is called with the input string as an argument.
+# The input() function can also take a list as an argument.
+# The list is displayed to the user before taking input.
+# The input() function can also take a tuple as an argument.
+# The tuple is displayed to the user before taking input.
+# The input() function can also take a set as an argument.
+# The set is displayed to the user before taking input.
+# The input() function can also take a dictionary as an argument.
+# The dictionary is displayed to the user before taking input.
+# The input() function can also take a string as an argument.
