26395bb1f25a5dbf7dd529ede4667548439ff8b1
Abdulsamad <you@example.com/usr/bin/python>
2025-04-16 14:00:23 +0100

    Remove unnecessary code for counting letters in a word

---
+++ Python Classes/len_function.py
@@ -0,0 +1,22 @@
+print(hello)
+num_char =len("hello")
+print(num_char)
+
+
+
+def my_function():
+    print("Hello")
+    print("Bye")
+    
+    
+ 
+ 
+ def my_function():
+     #do this 
+     #then this
+     #and this again
+     
+        
+    
+    
+
