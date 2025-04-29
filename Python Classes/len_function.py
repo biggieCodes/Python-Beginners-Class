beba0d523a1867bbae80b7ec32b6b84610f9c483
Abdulsamad <you@example.com/usr/bin/python>
2025-04-29 18:20:05 +0100

    Fix len function and remove unnecessary lines

--- Python Classes/len_function.py
+++ Python Classes/len_function.py
@@ -1,17 +1,14 @@
+hello = "Hello, World!"
 print(hello)
-num_char =len("hello")
+num_char = len("hello")
 print(num_char)
-
-
 
 def my_function():
     print("Hello")
     print("Bye")
-    
-    
-    
- 
+
 my_function()
+
 
 
      
